c4eb3c96db6c7dd7ba3efaf9af41e28968506991
edine-noella <edinenoella@gmail.com>
2025-03-25 16:27:34 +0200

    feature: added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div className="text-center py-20">
+        <h1 className="text-4xl font-bold text-green-700">Welcome to EcoFlow</h1>
+        <p className="mt-4 text-gray-600">Scan waste, find disposal stations, and earn rewards for eco-friendly actions.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
