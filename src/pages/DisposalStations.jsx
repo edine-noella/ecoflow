8bda09ee5c06d4687565baa70a9ef7e2c16767e6
edine-noella <edinenoella@gmail.com>
2025-03-25 16:30:18 +0200

    ft: added page to locate waste dispasal stations

---
+++ src/pages/DisposalStations.jsx
@@ -0,0 +1,11 @@
+function DisposalStations() {
+    return (
+      <div className="text-center py-20">
+        <h1 className="text-3xl font-bold text-green-700">Find Disposal Stations</h1>
+        <p className="mt-4 text-gray-600">Use GPS to locate the nearest disposal bins.</p>
+      </div>
+    );
+  }
+  
+  export default DisposalStations;
+  
